de63af70562bde2323d59dc8fcefd0b1c6ff0cb7
DoodMac <48062579+jbird84@users.noreply.github.com>
2021-05-19 06:46:48 -0500

    Initial Commit

---
+++ SwiftUISearchBar/SwiftUISearchBarApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUISearchBarApp.swift
+//  SwiftUISearchBar
+//
+//  Created by Kinney Kare on 5/19/21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUISearchBarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
